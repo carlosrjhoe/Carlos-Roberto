b30d78d53e335792718d88a01c7b9639516c1d3c
Carlos Roberto <carlosrjhoe@gmail.com>
2021-10-09 19:55:47 -0300

    _091021_2000

--- Exercícios/terreno.py
+++ Exercícios/terreno.py
@@ -2,11 +2,16 @@ largura = float(input('Largura do terreno: '))
 comprimento = float(input('Comprimento do terreno: '))
 valor = float(input('Valor do terreno: R$ '))
 
-def area(largura, comprimento):
-    return largura * comprimento
+class terreno():
 
-def preco(area, valor):
-    return area(largura, comprimento) * valor
+    def area(largura, comprimento):
+        return largura * comprimento
 
-print(f'\nArea do terreno = {area(largura, comprimento):.2f}m²')
-print(f'Preço do terreno = R${preco(area, valor):.2f}')
+    def preco(area, valor):
+        return area(largura, comprimento) * valor
+
+print(f'\nArea do terreno = {terreno.area(largura, comprimento):.2f}m²')
+print(f'Preço do terreno = R${terreno.preco(terreno.area, valor):.2f}')
+
+#print(area(largura, comprimento))
+#print(preco(area, valor))
