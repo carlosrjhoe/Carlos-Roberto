dc61d489d2a64dcb5cc74063fbcbbd1c7ce74d7e
Carlos Roberto <carlosrjhoe@gmail.com>
2021-10-10 14:51:10 -0300

    _101021_1450

--- Exercícios_condicionais/operadora.py
+++ Exercícios_condicionais/operadora.py
@@ -5,11 +5,14 @@ print('#' * 45)
 
 minutos = int(input('DIGITE A QUANTIDADE DE MINUTOS: '))
 
-plano = 50
+plano = 50.00
 multa = 2
 
-if minutos <= 100:
-    print(f'VALOR A PAGAR: R${plano}')
-else:
-    print(f'VALOR A PAGAR: R${((minutos - 100) * multa) + plano:.2f}')
+class operadora():
+    def valorApagar(minutos):
+        return ((minutos - 100) * 2) + plano
 
+if minutos < 100:
+    print(f'VALOR A PAGAR = R${plano:.2f}')
+else:
+    print(f'VALOR A PAGAR = R${operadora.valorApagar(minutos):.2f}')
