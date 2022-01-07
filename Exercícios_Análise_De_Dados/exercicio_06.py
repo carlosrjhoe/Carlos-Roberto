bd97002c743be7109c74a09a122b3a4045c6c7f5
Carlos Roberto <carlosrjhoe@gmail.com>
2022-01-06 21:35:02 -0300

    exercicio06

---
+++ Exercícios_Análise_De_Dados/exercicio_06.py
@@ -0,0 +1,6 @@
+# Exercício 7 - Usando a função filter(), encontre os valores que são comuns às duas listas abaixo.
+
+a = [1,2,3,5,7,9]
+b = [2,3,5,6,7,8]
+
+print (list(filter(lambda x: x in a, b)))   # VAI RETORNAR OS NUMEROS SEMELHANTES ENTRE AS DUAS LISTAS
